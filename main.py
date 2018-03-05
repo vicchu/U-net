3d31766b3de4a933c54c2d1db187dc402fcb3f00
anxiang <1363090802@qq.com>
2018-03-05 14:58:20 +0800

    modify

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ if __name__ == '__main__':
     }
 
 
+    
+
+
     data = Dataprovider(time=5)
     model = net.Model(dataprovider=data,loss_name='focal_loss',model_kw=model_kw)
     train = net.Train(dataprovider=data, model=model, batch_size=32)
