0c22940a4ae795c3d376bc505cfb15c5a66f0eaf
安翔君 <31175974+anxiangSir@users.noreply.github.com>
2018-04-02 10:41:53 +0800

    Update unet.py

--- models/unet.py
+++ models/unet.py
@@ -10,12 +10,10 @@ def unet(x, batch_norm=True, n_class=5, features=16):
     with tf.name_scope('1.unit'):
         w1_1 = weight_variable([3, 3, 3, features])
         b1_1 = bias_variable([features])
-        # net = conv2d_activtion(x, w1_1, b1_1, batch_norm)
-        net = conv2d_atrous(x, w1_1, b1_1, batch_norm)
+        net = conv2d_activtion(x, w1_1, b1_1, batch_norm)
         w1_2 = weight_variable([3, 3, features, features])
         b1_2 = bias_variable([features])
-        # net = conv2d_activtion(net, w1_2, b1_2, batch_norm)
-        net = conv2d_atrous(net, w1_2, b1_2, batch_norm)
+        net = conv2d_activtion(net, w1_2, b1_2, batch_norm)
         crop_and_concat1 = net
         net = max_pool(net)
 
@@ -23,12 +21,10 @@ def unet(x, batch_norm=True, n_class=5, features=16):
     with tf.name_scope('2.unit'):
         w2_1 = weight_variable([3, 3, features//2, features])
         b2_1 = bias_variable([features])
-        # net = conv2d_activtion(net, w2_1, b2_1, batch_norm)
-        net = conv2d_atrous(net, w2_1, b2_1, batch_norm)
+        net = conv2d_activtion(net, w2_1, b2_1, batch_norm)
         w2_2 = weight_variable([3, 3, features, features])
         b2_2 = bias_variable([features])
-        # net = conv2d_activtion(net, w2_2, b2_2, batch_norm)
-        net = conv2d_atrous(net, w2_2, b2_2, batch_norm)
+        net = conv2d_activtion(net, w2_2, b2_2, batch_norm)
         crop_and_concat2 = net
         net = max_pool(net)
 
@@ -36,46 +32,38 @@ def unet(x, batch_norm=True, n_class=5, features=16):
     with tf.name_scope('3.unit'):
         w3_1 = weight_variable([3, 3, features//2, features])
         b3_1 = bias_variable([features])
-        # net = conv2d_activtion(net, w3_1, b3_1, batch_norm)
-        net = conv2d_atrous(net, w3_1, b3_1, batch_norm)
+        net = conv2d_activtion(net, w3_1, b3_1, batch_norm)
         w3_2 = weight_variable([3, 3, features, features])
         b3_2 = bias_variable([features])
 
-        # net = conv2d_activtion(net, w3_2, b3_2, batch_norm)
-        net = conv2d_atrous(net, w3_2, b3_2, batch_norm)
+        net = conv2d_activtion(net, w3_2, b3_2, batch_norm)
         crop_and_concat3 = net
 
         net = max_pool(net)
 
-    # features = features*2
-    # with tf.name_scope('4.unit'):
-    #     w4_1 = weight_variable([3, 3, features//2, features])
-    #     b4_1 = bias_variable([features])
-    #     net = conv2d_activtion(net, w4_1, b4_1, batch_norm)
-    #     w4_2 = weight_variable([3, 3, features, features])
-    #     b4_2 = bias_variable([features])
-    #     net = conv2d_activtion(net, w4_2, b4_2, batch_norm)
-    #     crop_and_concat4 = net
-    #     net = max_pool(net)
-
-
-
-    with tf.name_scope('5.unit'):
-        net = pyramid_pooling_module(net, features, [2, 4, 8, 16])
-        features = features*2
+    features = features*2
+    with tf.name_scope('4.unit'):
+        w4_1 = weight_variable([3, 3, features//2, features])
+        b4_1 = bias_variable([features])
+        net = conv2d_activtion(net, w4_1, b4_1, batch_norm)
+        w4_2 = weight_variable([3, 3, features, features])
+        b4_2 = bias_variable([features])
+        net = conv2d_activtion(net, w4_2, b4_2, batch_norm)
+        crop_and_concat4 = net
+        net = max_pool(net)
 
     'up'
-    # features = features//2
-    # with tf.name_scope('6.unit'):
-    #     wd4_1 = weight_variable([2, 2, features, features*2])
-    #     bd4_1 = bias_variable([features])
-    #     net = deconv2d_concat(net, wd4_1, bd4_1, crop_and_concat4)
-    #     wd4_2 = weight_variable([3, 3, features*2, features])
-    #     bd4_2 = bias_variable([features])
-    #     net = conv2d_activtion(net, wd4_2, bd4_2, batch_norm)
-    #     wd4_3 = weight_variable([3, 3, features, features])
-    #     bd4_3 = bias_variable([features])
-    #     net = conv2d_activtion(net, wd4_3, bd4_3, batch_norm)
+    features = features//2
+    with tf.name_scope('6.unit'):
+        wd4_1 = weight_variable([2, 2, features, features*2])
+        bd4_1 = bias_variable([features])
+        net = deconv2d_concat(net, wd4_1, bd4_1, crop_and_concat4)
+        wd4_2 = weight_variable([3, 3, features*2, features])
+        bd4_2 = bias_variable([features])
+        net = conv2d_activtion(net, wd4_2, bd4_2, batch_norm)
+        wd4_3 = weight_variable([3, 3, features, features])
+        bd4_3 = bias_variable([features])
+        net = conv2d_activtion(net, wd4_3, bd4_3, batch_norm)
 
     features = features//2
     with tf.name_scope('7.unit'):
@@ -141,8 +129,3 @@ def pyramid_pooling_module(input, features, poolsizes):
     return net
 
 
-a = tf.constant(1.0,shape=[10,256,256,3])
-b = unet(a,batch_norm=True,n_class=5,features=16)
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-print(sess.run(b))
